9f631fae6c8fcbf4b6c4a4b947df804372d3aca7
Samuel Aroney <sam@zooniverse.org>
2018-10-09 16:26:57 +0100

    Add field descriptions

--- app/graphql/types/event_type.rb
+++ app/graphql/types/event_type.rb
@@ -2,11 +2,16 @@ module Types
   class EventType < GraphQL::Schema::Object
     description 'The Event type'
     # event specific fields
-    field :event_id, ID, null: false
-    field :event_type, String, null: true
-    field :event_time, GraphQL::Types::ISO8601DateTime, null: false
-    field :event_created_at, GraphQL::Types::ISO8601DateTime, null: true
-    field :event_source, String, null: false
+    field :event_id, ID, null: false,
+    description: "Event ID"
+    field :event_type, String, null: true,
+    description: "Type of event (classification, comment, ...)"
+    field :event_time, GraphQL::Types::ISO8601DateTime, null: false,
+    description: "Timestamp of event"
+    field :event_created_at, GraphQL::Types::ISO8601DateTime, null: true,
+    description: "Time database entry created"
+    field :event_source, String, null: false,
+    description: "Source of event"
     
     # user fields
     field :user_id, ID, null: true,
@@ -15,27 +20,45 @@ module Types
     description: "Global user ID"
     field :zooniverse_id, ID, null: true,
     description: "Global ID"
-    field :lang, String, null: true
-    field :user_agent, String, null: true
-    field :user_name, String, null: true
+    field :lang, String, null: true,
+    description: "Client language"
+    field :user_agent, String, null: true,
+    description: "Client web-browser info"
+    field :user_name, String, null: true,
+    description: "Zooniverse username"
     
     # project fields
-    field :project_id, ID, null: true
-    field :project_name, String, null: true
-    field :workflow_id, ID, null: true
-    field :subject_ids, String, null: true
-    field :subject_urls, String, null: true
+    field :project_id, ID, null: true,
+    description: "Project ID"
+    field :project_name, String, null: true,
+    description: "Project name"
+    field :workflow_id, ID, null: true,
+    description: "Workflow ID under project"
+    field :subject_ids, String, null: true,
+    description: "Subject ID under project"
+    field :subject_urls, String, null: true,
+    description: "Urls for each subject"
     
     # discussion board fields
-    field :board_id, ID, null: true
-    field :board, String, null: true
-    field :discussion_id, ID, null: true
-    field :focus, String, null: true
-    field :focus_id, ID, null: true
-    field :focus_type, String, null: true
-    field :section, String, null: true
-    field :body, String, null: true
-    field :url, String, null: true
-    field :tags, String, null: true
+    field :board_id, ID, null: true,
+    description: "Board ID"
+    field :board, String, null: true,
+    description: "Board name"
+    field :discussion_id, ID, null: true,
+    description: "Discussion ID"
+    field :focus, String, null: true,
+    description: "Comment focus"
+    field :focus_id, ID, null: true,
+    description: "Focus ID"
+    field :focus_type, String, null: true,
+    description: "Comment focus type"
+    field :section, String, null: true,
+    description: "Comment section"
+    field :body, String, null: true,
+    description: "Body of comment"
+    field :url, String, null: true,
+    description: "Url for comment"
+    field :tags, String, null: true,
+    description: "Comment tags"
   end
 end

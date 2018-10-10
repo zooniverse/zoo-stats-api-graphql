2259f476a8b0a664de773a3cf472a8e9b2172cc0
Samuel Aroney <sam@zooniverse.org>
2018-10-10 16:45:31 +0100

    Add query by projectId, returning all events by project
    Return fields as defined by events type
    Tested with ZooStatsSchema.execute and manually by
    GraphiQL

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,12 +2,21 @@ module Types
   class QueryType < Types::BaseObject
     description 'The query root of this schema'
     field :user_id_query, [Types::EventType], null: false do
-      description 'returns events with user_id'
+      description 'returns all events with user_id'
       argument :userId, ID, required: true
     end
 
     def user_id_query(user_id:)
       Event.where(user_id: user_id)
+    end
+
+    field :project_id_query, [Types::EventType], null: false do
+      description 'returns all events with project_id'
+      argument :project_id, ID, required: true
+    end
+
+    def project_id_query(project_id:)
+      Event.where(project_id: project_id)
     end
   end
 end

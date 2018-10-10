71d6f07fdb57569c3bcfc0f290f849a2de7cb965
Samuel Aroney <sam@zooniverse.org>
2018-10-10 14:31:25 +0100

    Add query by userId, returning all events by user
    Return fields as defined by events type
    Tested via ZooStatsSchema.execute

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,13 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    description 'The query root of this schema'
+    field :user_id_query, [Types::EventType], null: false do
+      description 'returns events with user_id'
+      argument :userId, ID, required: true
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def user_id_query(user_id:)
+      Event.where(user_id: user_id)
     end
   end
 end

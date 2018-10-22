f0e4fe569372397fa5d20da5b36fa81b4e49925d
Samuel Aroney <sam@zooniverse.org>
2018-10-22 14:02:56 +0100

    Add event generation to ruby testing script
    Want to run concurrently with test suite to more accurately
    match database usage

--- scripts/generate_testing_database/run_database_tests.rb
+++ scripts/generate_testing_database/run_database_tests.rb
@@ -1,5 +1,41 @@
 require "benchmark"
+require_relative "generate_events"
 
-puts "test1"
-puts "test2"
-puts "test3"
+puts "test01"
+puts "test02"
+puts "test03"
+events_generator = Utilities.new
+events_generator.event_id = 100_000
+events_generator.generate_event
+
+=begin
+test_block = 
+
+4.times do
+  puts test_block
+  time = Benchmark.measure do
+    results = Event.where(event_id: [1, 2, 3, 4])
+  end
+  puts results
+  puts time
+end
+=end
+
+=begin
+def timing_test(title)
+  puts title
+  ["cold", 1, 2, 3].each do |run|
+    puts run
+    time = Benchmark.measure { yield }
+    puts time
+  end
+end
+
+[
+  "Multiple where":       { Event.where(event_id: [1, 2, 3, 4]) },
+  "Ordered selection":    {     Event.limit(5).offset(30) },
+  "Where and count":      { Event.where(user_id: 1).group("project_id").count }
+]
+
+
+=end

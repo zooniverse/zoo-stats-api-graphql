52820f63568acdd8e46dcbd057d9176842cb1e35
Samuel Aroney <sam@zooniverse.org>
2018-10-22 14:02:45 +0100

    Change to db:reset at start of test

--- testing_script.sh
+++ testing_script.sh
@@ -9,19 +9,20 @@ script_path="./scripts/generate_testing_database/"
 
 echo "***Script starting***"
 echo "***Setup database***"
-docker-compose run --rm --entrypoint="bundle exec rake db:setup" zoo_stats
+docker-compose run --rm --entrypoint="bundle exec rake db:reset" zoo_stats
 
 for i in {0..1}; do
-    echo "***Database size ${sizes[$i]}***"
+    echo "|||Database size ${sizes[$i]}|||"
     echo "***Importing data***"
     docker cp $path${file_array[$i]}.csv zoo_stats_api_prototype_timescale_1:/input.csv
     docker cp ${script_path}add_csv.sql zoo_stats_api_prototype_timescale_1:/input.sql
     docker exec zoo_stats_api_prototype_timescale_1 psql -U zoo_stats zoo_stats_development -f input.sql
 
-    echo "***Running database tests***"
+    echo "***Test setup***"
     docker stop zoo_stats_api_prototype_timescale_1
     docker start zoo_stats_api_prototype_timescale_1
     output_path=$path${file_array[$i]}_output.csv
     > $output_path
+    echo "***Running database tests***"
     docker-compose run --rm --entrypoint="bin/rails runner scripts/generate_testing_database/run_database_tests.rb" zoo_stats >> $output_path
 done

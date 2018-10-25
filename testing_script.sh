c2be87cd866d5b9677f6440323b7f26b4034c74f
Samuel Aroney <sam@zooniverse.org>
2018-10-25 10:45:38 +0100

    Refactor testing_script dynamic options
    Run ./testing_script.sh {repeats} { yes / no }
    Procfile: moved testing execution to after writers

--- testing_script.sh
+++ testing_script.sh
@@ -5,14 +5,21 @@ sizes=(100K 200K 300K 400K 500K 750K 1M 2M 5M 10M 20M 50M 100M 500M 1B)
 sizes_i=(100000 200000 300000 400000 500000 750000 1000000 2000000 5000000 10000000 20000000 50000000 100000000 500000000 1000000000)
 path="/Users/samuelaroney/Data/"
 script_path="./scripts/generate_testing_database/"
-repeats=1000
-writes="yes"
+temp_file="./testing_results/temp"
+
+repeats=$1
+if [ "$2" = "no" ]
+then
+    writes="no"
+else
+    writes="yes"
+fi
 output_file="./testing_results/test_output-${repeats}-${writes}_writes.csv"
 
 # List of inputs to use
 quick_list=(0 1)
 start_list=(0 1 2 9)
-extra_list=(0 1 2 9 10 11 14)
+extra_list=(0 1 2 9 11 12)
 all_list=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14)
 
 echo "***Script setup***"
@@ -36,9 +43,9 @@ do
     database_size=${sizes_i[$i]}
     if [ $writes = "yes" ]
     then
-        docker-compose run -e repeats=$repeats -e database_size=$database_size --rm --entrypoint="bundle exec foreman start" zoo_stats
-        # cat ./testing_results/temp >> $output_file
-        # rm ./testing_results/temp
+        docker-compose run -e repeats=$repeats -e database_size=$database_size -e temp_file=$temp_file --rm --entrypoint="bundle exec foreman start" zoo_stats
+        cat $temp_file >> $output_file
+        rm $temp_file
     else
         docker-compose run -e repeats=$repeats -e database_size=$database_size --rm --entrypoint="bin/rails runner scripts/generate_testing_database/run_database_tests.rb" zoo_stats >> $output_file
     fi

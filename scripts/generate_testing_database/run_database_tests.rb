77ed5743ea29156a416949d5e6555351493a77cf
Samuel Aroney <sam@zooniverse.org>
2018-10-30 14:43:17 +0000

    Change to sql activerecord load time
    More accurate view of database request return time
    Monitors ActiveSupport::Notifications for those tagged
    'sql.active_record' that are parsed for 'Event Load' notifications,
    those corresponding to my query.
    
    Possible addition of separation between these query times.

--- scripts/generate_testing_database/run_database_tests.rb
+++ scripts/generate_testing_database/run_database_tests.rb
@@ -14,23 +14,55 @@ repeat_count = repeat_count.to_i
 time_value     = 1
 time_units     = 'seconds'
 
+# Setup notification subscription
+records = []
+ActiveSupport::Notifications.subscribe('sql.active_record') do |*args|
+  record = ActiveSupport::Notifications::Event.new(*args)
+  record.duration
+  records << record
+end
+
 # test runs
 cold = "True"
 repeat_count.times do
-  time = Benchmark.measure do
-    test_user_id = rand(1..1_000_000)
-    time_bucket = Event.select("time_bucket('#{time_value} #{time_units}', event_time) AS bucket, count(*)").group("bucket")
-    time_bucket.where(user_id: test_user_id, event_type: "classification").load
-    time_bucket.where(user_id: test_user_id, event_type: "comment").load
+  current = records.length
+
+  test_user_id = rand(1..1_000_000)
+  time_bucket = Event.select("time_bucket('#{time_value} #{time_units}', event_time) AS bucket, count(*)").group("bucket")
+  time_bucket.where(user_id: test_user_id, event_type: "classification").load
+  time_bucket.where(user_id: test_user_id, event_type: "comment").load
+
+  time = 0
+  (current..records.length - 1).each do |i|
+    if records[i].payload[:name] == "Event Load"
+      time += records[i].duration / 1000
+    end
   end
-  puts "#{database_size},#{time.real.round(5)},#{cold}"
+  puts "#{database_size},#{time.round(5)},#{cold}"
   cold = "False"
 end
 
 # output CSV layout
 # database_size,time,cold
 
+=begin
+time_value = 1
+time_units = 'seconds'
+test_user_id = rand(1..1_000_000)
+test_project_id = rand(1..1_000)
+Event.select("time_bucket('#{time_value} #{time_units}', event_time) AS bucket, count(*)").group("bucket").where(user_id: test_user_id, event_type: "classification").load
+Event.select("time_bucket('#{time_value} #{time_units}', event_time) AS bucket, count(*)").group("bucket").where(user_id: test_user_id, event_type: "comment").load
+Event.select("time_bucket('#{time_value} #{time_units}', event_time) AS bucket, count(*)").group("bucket").where(project_id: test_project_id, event_type: "classification").load
+Event.select("time_bucket('#{time_value} #{time_units}', event_time) AS bucket, count(*)").group("bucket").where(project_id: test_project_id, event_type: "comment").load
 
+records = []
+ActiveSupport::Notifications.subscribe('sql.active_record') do |*args|
+  record = ActiveSupport::Notifications::Event.new(*args)
+  puts record.duration
+  puts record.payload[:name]
+  records << record
+end
+=end
 
 ## ActiveRecord queries
 

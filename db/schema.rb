3e9f143dda39939cc5594889d3b28929414f906e
Samuel Aroney <sam@zooniverse.org>
2018-10-31 14:37:34 +0000

    Add composite indexes
    event_type with user_id
    event_type with project_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_140257) do
+ActiveRecord::Schema.define(version: 2018_10_30_101442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2018_10_04_140257) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_time"], name: "events_event_time_idx", order: :desc
+    t.index ["event_type", "project_id"], name: "index_events_on_event_type_and_project_id"
+    t.index ["event_type", "user_id"], name: "index_events_on_event_type_and_user_id"
   end
 
 end

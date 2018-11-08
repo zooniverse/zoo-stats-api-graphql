d9f9b1776afe63e0e47af22670a68361ba513cdc
Samuel Aroney <sam@zooniverse.org>
2018-11-08 15:36:15 +0000

    Add health check for get '/'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def health
+    render json: {status: 'ok', version: VERSION}.to_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+require_relative '../version'
+
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/", to: "application#health"
 end

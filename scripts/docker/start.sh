23dc32d30388049eb4477c4afe1de7e23c16fdf5
Campbell Allen <campbell.allen@gmail.com>
2018-11-09 14:30:55 +0000

    update travis to run rspec

--- scripts/docker/start.sh
+++ scripts/docker/start.sh
@@ -1,11 +1,7 @@
 #!/bin/bash -ex
-
 cd /rails_app
-
-tmpreaper 7d /tmp/
 
 mkdir -p tmp/pids/
 rm -f tmp/pids/*.pid
 
-bundle check || bundle install --binstubs="$BUNDLE_BIN"
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf

0a59d2b5d01df9d69f833db744c10e5674a5b795
AroneyS <41124903+AroneyS@users.noreply.github.com>
2018-11-15 13:03:30 +0000

    Add database health check (#26)
    * Add database health check
    
    * Return database_status as connected
    
    Any errors in connection will be sent to Rollbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   def health
-    render json: {status: 'ok', version: VERSION}.to_json
+    database_status = ActiveRecord::Base.connection.execute("SELECT 1 FROM events")
+    render json: {status: "ok", version: VERSION, database_status: "connected"}.to_json
   end
 end

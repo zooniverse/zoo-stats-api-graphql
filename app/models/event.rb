c8c8ed7fee87220af0b1a04a984b102f3f90ec9b
Samuel Aroney <sam@zooniverse.org>
2018-11-20 11:54:00 +0000

    Add event_type validation to model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,5 @@ class Event < ApplicationRecord
   validates :event_id, presence: true
   validates :event_source, presence: true
   validates :event_time, presence: true
+  validates :event_type, presence: true
 end

50969e8c1604e6e33155aeae305a7c1a8fa2c092
Samuel Aroney <sam@zooniverse.org>
2018-11-20 12:10:28 +0000

    Change primary key order

--- db/migrate/20181120113739_add_typeto_events_primary_key.rb
+++ db/migrate/20181120113739_add_typeto_events_primary_key.rb
@@ -4,7 +4,7 @@ class AddTypetoEventsPrimaryKey < ActiveRecord::Migration[5.2]
       dir.up do
         execute <<-SQL
           ALTER TABLE events DROP CONSTRAINT events_pkey;
-          ALTER TABLE events ADD CONSTRAINT events_pkey PRIMARY KEY (event_id, event_source, event_time, event_type);
+          ALTER TABLE events ADD CONSTRAINT events_pkey PRIMARY KEY (event_id, event_type, event_source, event_time);
         SQL
       end
     end

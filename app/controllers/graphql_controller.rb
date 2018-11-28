8c484ed4202d059e2cc4fb58b7e0445dc0f6d0db
Samuel Aroney <sam@zooniverse.org>
2018-11-28 15:07:45 +0000

    Add basic http auth to context

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,7 +1,7 @@
 require_relative '../graphql/utilities/credential'
 
 class GraphqlController < ApplicationController
-  attr_reader :credential
+  attr_reader :credential, :basic_credential
   before_action :setup_and_validate_credential
 
   def execute
@@ -10,7 +10,9 @@ class GraphqlController < ApplicationController
     operation_name = params[:operationName]
     context = {
       current_user: current_user,
-      admin: admin_status
+      admin: admin_status,
+      basic_user: basic_user,
+      basic_password: basic_password
     }
     result = ZooStatsSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
@@ -28,14 +30,25 @@ class GraphqlController < ApplicationController
     credential.current_admin_status if credential
   end
 
+  def basic_user
+    basic_credential[0] if basic_credential
+  end
+
+  def basic_password
+    basic_credential[1] if basic_credential
+  end
+
   def setup_and_validate_credential
     authorization = request.headers['HTTP_AUTHORIZATION']
-    match = /\ABearer (.*)\Z/.match(authorization)
-
-    return unless match
-    auth = match[1]
-    temp = Credential.new(auth)
-    @credential = temp if temp.ok?
+    
+    if match = /\ABearer (.*)\Z/.match(authorization)
+      auth = match[1]
+      temp = Credential.new(auth)
+      @credential = temp if temp.ok?
+    end
+    if match = /\ABasic (.*)\Z/.match(authorization)
+      @basic_credential = ActionController::HttpAuthentication::Basic::user_name_and_password(request)
+    end
   end
 
   # Handle form data, JSON body, or a blank value

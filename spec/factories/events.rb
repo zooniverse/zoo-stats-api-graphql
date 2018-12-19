259de9899655279be13f20197bd48ec817479445
Campbell Allen <campbell.allen@gmail.com>
2018-12-19 14:17:14 +0000

    Spec upserts (#58)
    * move factory and add more event details
    
    * switch to dynamic attributes
    
    remove deprecation warning
    
    * add epsec for upserts
    
    * allow filter spec runs
    
    * add a gemset file for rvm
    
    * ensure the model exists for find spec

---
+++ spec/factories/events.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :event do
+    sequence(:event_id)     { |n| "100#{n}".to_i }
+    sequence(:event_source) { |n| "Server ##{n}" }
+    sequence(:event_time)   { Time.now.round(0) }
+    sequence(:event_type)   { "background" }
+    sequence(:project_id)   { |n| "200#{n}".to_i }
+    sequence(:workflow_id)  { |n| "300#{n}".to_i }
+    sequence(:user_id)      { |n| "400#{n}".to_i }
+    data                    { {"metadata" => "test"} }
+    session_time            { rand(1.0..10.0) }
+    country_name { "United Kingdom" }
+    country_code { "GB" }
+    city_name { "Oxford" }
+    latitude { 51.75222 }
+    longitude { -1.25596 }
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,6 +44,8 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
+  config.filter_run_when_matching :focus
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
@@ -53,6 +55,7 @@ RSpec.configure do |config|
   # aliases for `it`, `describe`, and `context` that include `:focus`
   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
   config.filter_run_when_matching :focus
+
 
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend

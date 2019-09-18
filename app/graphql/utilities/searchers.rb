8ab19d066c5d8dca5387daace70fd7ec62481420
Campbell Allen <campbell.allen@gmail.com>
2019-09-18 14:49:25 +0100

    add window param to searcher
    limit the window of data we are looking for, i.e. the last 7 days of data, window='7 days'

--- app/graphql/utilities/searchers.rb
+++ app/graphql/utilities/searchers.rb
@@ -1,8 +1,14 @@
 module Searchers
   class Bucket
-    def self.search(kwargs)
-      time_bucket = kwargs.delete(:interval)
-      Event.select("time_bucket('#{time_bucket}', event_time) AS period, count(*)").group("period").where(**kwargs)
+    def self.search(query_filters)
+      time_bucket = query_filters.delete(:interval)
+      window = query_filters.delete(:window)
+      Event.select(
+          "time_bucket('#{time_bucket}', event_time) AS period, count(*)"
+        )
+        .group("period")
+        .where(**query_filters)
+        .where("event_time >= NOW() - INTERVAL ?", window)
     end
   end
 end
